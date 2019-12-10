aaf9dfaee006293fbc6b7f919cd04e3ff225a895
QiangYin1991 <qiangyin1991@gmail.com>
2019-12-10 23:53:10 +0800

    prob1

--- problem0001/main.go
+++ problem0001/main.go
@@ -2,6 +2,48 @@ package main
 
 import "fmt"
 
+func twoSum(nums []int, target int) []int {
+	var res []int
+	hashMap := map[int]int{}
+	for idx, val := range nums {
+		hashMap[val] = idx
+	}
+
+	for idx, src := range nums {
+		dst := target - src
+		_, exist := hashMap[dst]
+		if exist && hashMap[dst] != idx {
+			res = append(res, idx)
+			res = append(res, hashMap[dst])
+			break
+		}
+	}
+	return res
+}
+
+func twoSum2(nums []int, target int) []int {
+	res := []int{}
+	hashMap := map[int]int{}
+	for idx := len(nums) - 1; idx >= 0; idx-- {
+		dst := target - nums[idx]
+		_, exist := hashMap[dst]
+		if exist {
+			res = append(res, idx)
+			res = append(res, hashMap[dst])
+			break
+		} else {
+			hashMap[nums[idx]] = idx
+		}
+	}
+	return res
+}
+
 func main() {
-	fmt.Println("Hello World");
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	res := twoSum2(nums, target)
+
+	for _, val := range res {
+		fmt.Printf("%v\n", val)
+	}
 }
